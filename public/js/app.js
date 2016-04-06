0a2e993e64105dc850f851ef885dd7b3f43fadeb
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-04-05 17:17:04 -0700

    added views to partials via angular states

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,38 @@
     .config(function($httpProvider){
       $httpProvider.interceptors.push('authInterceptor');
     })
+    .config(function($stateProvider, $urlRouterProvider){
+
+      //handle any attempts to routes other than what is listed below
+      // $urlRouterProvider.otherwise('/')
+
+      //my established routes
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: '/partials/home.html'
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: '/partials/login.html'
+        })
+        .state('signup', {
+          url: '/signup',
+          templateUrl: '/partials/signup.html'
+        })
+        .state('users', {
+          url: '/users',
+          templateUrl: '/partials/users.html'
+        })
+        .state('profile', {
+          url: '/profile',
+          templateUrl: '/partials/profile.html'
+        })
+        .state('begin', {
+          url: '/begin',
+          templateUrl: '/partials/begin.html'
+        })
+      })
 
 
 })()
